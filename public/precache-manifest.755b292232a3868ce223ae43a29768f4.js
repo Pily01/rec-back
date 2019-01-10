ffabc1f8f392ec302a540a9a2f850c1189bc6a3e
Pily <paila@live.com.mx>
2019-01-10 02:01:58 -0600

    create new repo

---
+++ public/precache-manifest.755b292232a3868ce223ae43a29768f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "122978fa149452209302",
+    "url": "/static/js/main.122978fa.chunk.js"
+  },
+  {
+    "revision": "fdb9b3b30c75e399261f",
+    "url": "/static/js/1.fdb9b3b3.chunk.js"
+  },
+  {
+    "revision": "122978fa149452209302",
+    "url": "/static/css/main.5ee1ef97.chunk.css"
+  },
+  {
+    "revision": "6d6af132a91baf2e32d7921146d71d2c",
+    "url": "/index.html"
+  }
+];
